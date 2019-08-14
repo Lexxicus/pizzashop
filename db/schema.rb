3aaacf2c833a97dc3a7dc5f71b77900ce1be6d18
Lexxicus <alexxxicus@gmail.com>
2019-08-14 23:20:08 +0500

    seed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_164609) do
+ActiveRecord::Schema.define(version: 2019_08_14_171157) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
