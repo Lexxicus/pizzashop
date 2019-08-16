9c9e7a7a15c6c50be5c4bfb97e5bad42dc3296a0
Lexxicus <alexxxicus@gmail.com>
2019-08-16 19:51:47 +0500

    pass pizza id, to add_to_cart

--- public/app.js
+++ public/app.js
@@ -2,8 +2,14 @@ var x = 1
 
 function something()
 {
-    var x = window.localStorage.getItem('aaa');
-    x = x*1 + 1
-    window.localStorage.setItem('aaa', x);
-    alert(x);
+   // var x = window.localStorage.getItem('aaa');
+    //x = x*1 + 1
+    //window.localStorage.setItem('aaa', x);
+    //alert(x);
+    alert('Hello from function');
+}
+
+function add_to_cart(id)
+{
+    alert('you add pizza id: ' + id);
 }
