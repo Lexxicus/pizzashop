ddfd20faeef2f6abe0d19a8abf475d5e4a6b47b1
Lexxicus <alexxxicus@gmail.com>
2019-08-16 18:02:25 +0500

    seed db version II

--- db/migrate/20190814171157_add_products.rb
+++ db/migrate/20190814171157_add_products.rb
@@ -2,8 +2,35 @@
 
 class AddProducts < ActiveRecord::Migration[5.2]
   def change
-    Product.create title: 'Hawaiian', description: 'This is Hawaiian pizza', price: 350, size: 30, is_spicy: false, is_veg: false, is_best_offer: false, path_to_image: '/images/hawaiian.jpg'
-    Product.create title: 'Pepperoni', description: 'This is cool Pepperoni pizza', price: 400, size: 30, is_spicy: false, is_veg: false, is_best_offer: false, path_to_image: '/images/pepperoni.jpg'
-    Product.create title: 'Vegetarian', description: 'This is tereble Vegetarian pizza', price: 350, size: 30, is_spicy: false, is_veg: false, is_best_offer: true, path_to_image: '/images/veg.jpg'
+    Product.create ({
+        title: 'Hawaiian',
+        description: 'This is Hawaiian pizza',
+        price: 350,
+        size: 30,
+        is_spicy: false,
+        is_veg: false,
+        is_best_offer: false,
+        path_to_image: '/images/hawaiian.jpg'
+        })
+    Product.create ({
+        title: 'Pepperoni',
+        description: 'This is cool Pepperoni pizza',
+        price: 400,
+        size: 30,
+        is_spicy: false,
+        is_veg: false,
+        is_best_offer: false,
+        path_to_image: '/images/pepperoni.jpg'
+        })
+    Product.create ({
+        title: 'Vegetarian',
+        description: 'This is tereble Vegetarian pizza',
+        price: 350,
+        size: 30,
+        is_spicy: false,
+        is_veg: false,
+        is_best_offer: true,
+        path_to_image: '/images/veg.jpg'
+        })
   end
 end
