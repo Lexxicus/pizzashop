6b0425af9f9b6495c8151b4378f15bce2786578d
Lexxicus <alexxxicus@gmail.com>
2019-08-19 23:45:48 +0500

    add all_Storage function

--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-var x = 1
-
 function something()
 {
    // var x = window.localStorage.getItem('aaa');
@@ -11,8 +9,30 @@ function something()
 
 function add_to_cart(id)
 {
-    var key = 'product_' + id //создаем переменную для ключа хэша
-    var x = window.localStorage.getItem(key); // записываем переменную в локал сторедж
-    x = x*1 + 1; // увеличиваем значение переменной
-    window.localStorage.setItem(key, x) // записываем значение пару ключ значение в локалсторедж
+    //создаем переменную для ключа хэша
+    var key = 'product_' + id;
+    // записываем переменную в локал сторедж
+    var x = window.localStorage.getItem(key);
+    // увеличиваем значение переменной
+    x = x*1 + 1;
+    // записываем значение пару ключ значение в локалсторедж
+    window.localStorage.setItem(key, x);
+}
+
+function all_Storage() {
+
+    var sum = 0;
+
+    for (var i = 0; i < localStorage.length; i++) {
+
+        // set iteration key name
+        var key = localStorage.key(i);
+
+        // use key name to retrieve the corresponding value
+        var value = localStorage.getItem(key)*1;
+        // get sum of all value of localstorage
+        sum = sum + value;
+
+    }
+    alert (sum);
 }
