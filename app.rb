88630f9095fb0e694903adf688775a0552a15db9
Lexxicus <alexxxicus@gmail.com>
2019-08-28 22:10:58 +0500

    display order information

--- app.rb
+++ app.rb
@@ -39,7 +39,11 @@ end
 post'/cart' do
 
   orders_input = params[:orders]
-  @orders = parse_orders_input orders_input
-  erb "Hello #{@orders}"
+  @items = parse_orders_input orders_input
+  @items.each do |item|
+    item[0] = Product.find(item[0])
+  end
+
+  erb :cart
 
 end
